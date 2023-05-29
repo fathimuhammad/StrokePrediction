ecb65fdfbc6c06c4b8ff412008131d427f573bf3
fathinawa <79491310+fathimuhammad@users.noreply.github.com>
2023-05-29 16:29:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"darekarabhishek@gmail.com\"\n\
+email = \"fathimuhammad.2020@student.uny.ac.id\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
